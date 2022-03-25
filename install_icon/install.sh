ff65ac83599637a68de51e720150d703c1b8cbc7
mbridak <michael.bridak@gmail.com>
2022-03-25 14:44:52 -0700

    copy binary to ./local/bin

--- install_icon/install.sh
+++ install_icon/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cp ../dist/winkeyerserial ~/.local/bin/
+
 xdg-icon-resource install --size 64 --context apps --mode user k6gte-pywinkey.png k6gte-pywinkey
 
 xdg-desktop-icon install k6gte-winkeyerserial.desktop
